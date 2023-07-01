1ce13b0575b3624accd12bc8e09ebc4820a41b5b
GaurangParmar1810 <202001444@daiict.ac.in>
2023-07-01 14:12:25 +0530

    testimonials section done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css'
 const Contact = () => {
   return (
     <section id="contact">
-      <h5>To discuss your skills and experience in more detail</h5>
+      <h5>To discuss your skills and experience in more detail </h5>
       <h2>Contact Me</h2>
     </section>
   )
