5679241921ec6a8d1d80cc00cee45483cb54de37
GaurangParmar1810 <202001444@daiict.ac.in>
2023-07-01 14:19:49 +0530

    navbar section edited

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
+import {GiAchievement } from 'react-icons/gi'
 import {FaLaptopCode} from 'react-icons/fa'
+import {VscFeedback} from 'react-icons/vsc'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 
 import { useState } from 'react';
@@ -13,8 +14,9 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <AiOutlineHome /> </a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}> <AiOutlineUser /> </a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <BiBook /> </a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <GiAchievement /> </a>
       <a href="#portfoliyo" onClick={() => setActiveNav('#portfoliyo')} className={activeNav === '#portfoliyo' ? 'active' : ''}> <FaLaptopCode /> </a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}> <VscFeedback /> </a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <BiMessageSquareDetail /> </a>
     </nav>
   )
