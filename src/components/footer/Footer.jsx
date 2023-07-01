5d0319dd5099d289d3c453b08eb8a2663e2ccc69
GaurangParmar1810 <202001444@daiict.ac.in>
2023-07-01 17:20:33 +0530

    navbar style changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './footer.css'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Gaurang Parmar</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfoliyo">Portfoliyo</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </footer>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,18 +6,62 @@ import {FaLaptopCode} from 'react-icons/fa'
 import {VscFeedback} from 'react-icons/vsc'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const [tootlTips, setTootlTips] = useState(null);
+
+  const handleMouseEnter = (e) => {
+    setTootlTips(e.target.title);
+  };
+
+  const handleMouseLeave = () => {
+    setTootlTips(null);
+  };
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setIsNavVisible(currentScrollPos > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <AiOutlineHome /> </a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}> <AiOutlineUser /> </a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <GiAchievement /> </a>
-      <a href="#portfoliyo" onClick={() => setActiveNav('#portfoliyo')} className={activeNav === '#portfoliyo' ? 'active' : ''}> <FaLaptopCode /> </a>
-      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}> <VscFeedback /> </a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <BiMessageSquareDetail /> </a>
+    <nav className={isNavVisible ? 'show' : ''}>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} title="Home" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> <AiOutlineHome /> 
+        <span className="tooltip">{tootlTips}</span>
+      </a>
+
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}  title="About Me" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> <AiOutlineUser /> 
+      <span className="tooltip">{tootlTips}</span>
+      </a>
+      
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} title="Skill-set" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> 
+        <GiAchievement /> 
+        <span className="tooltip">{tootlTips}</span>
+      </a>
+      
+      <a href="#portfoliyo" onClick={() => setActiveNav('#portfoliyo')} className={activeNav === '#portfoliyo' ? 'active' : ''} title="View Protfolio" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> 
+        <FaLaptopCode /> 
+        <span className="tooltip">{tootlTips}</span>
+      </a>
+      
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''} title="Take a look of Testinomials" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> 
+        <VscFeedback /> 
+        <span className="tooltip">{tootlTips}</span>
+      </a>
+      
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} title = "Contact Me" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> 
+        <BiMessageSquareDetail /> 
+        <span className="tooltip">{tootlTips}</span>
+      </a>
     </nav>
   )
 }
