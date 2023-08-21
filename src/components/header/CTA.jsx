1a9e1906a6f2b253148e8fa4abd42ef1a89aa585
GaurangParmar1810 <202001444@daiict.ac.in>
2023-08-21 22:04:31 +0530

    CV->Resume changed

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './header.css'
-import CV from '../../assets/Gaurang_Parmar_CV.pdf'
+import CV from '../../assets/Gaurang_Parmar_Resume.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'> Download CV</a>
+        <a href={CV} download className='btn'> Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's have some talk!</a>
     </div>
   )
